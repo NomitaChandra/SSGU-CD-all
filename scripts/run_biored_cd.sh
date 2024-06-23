5dd399c07e6fd2a00d65e48c893c7f5e5f3e8410
NNroc <1581333251@qq.com>
2024-06-23 20:47:32 +0800

    update

--- scripts/run_biored_cd.sh
+++ scripts/run_biored_cd.sh
@@ -2,15 +2,11 @@
 load_path=""
 losses=BSCELoss
 use_gcns=("both" "tree")
-seeds=(64 66 68 70 72)
 for loss in "${losses[@]}"
 do
   for use_gcn in "${use_gcns[@]}"
   do
-    for seed in "${seeds[@]}"
-    do
-      python train_bio.py --task biored_cd --loss $loss --use_gcn $use_gcn --seed $seed --load_path "$load_path"
-      sleep 100
-    done
+    python train_bio.py --task biored_cd --loss $loss --use_gcn $use_gcn --seed $seed --load_path "$load_path"
+    sleep 100
   done
 done
--- scripts/run_cdr.sh
+++ scripts/run_cdr.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
 
-load_path=""
-#load_path == /py_project/SSGU-CD/out/train_filter_bert_cdr_seed_best
-#losses=(BalancedLoss ATLoss AsymmetricLoss APLLoss)
 losses=BSCELoss
 use_gcns=("both" "mentions" "tree" "false")
-seeds=(64 66 68 70 72)
 for loss in "${losses[@]}"
 do
   for use_gcn in "${use_gcns[@]}"
   do
-    for seed in "${seeds[@]}"
-    do
     python train_bio.py --task cdr --loss $loss --use_gcn $use_gcn --seed $seed --load_path "$load_path"
     sleep 100
-    done
   done
 done
